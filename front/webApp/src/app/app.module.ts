abcba48f365ae834ad03d2035b446f747b067c2a
Martin Del Ollio <110709751+martindelollio@users.noreply.github.com>
2024-10-26 20:27:11 -0300

    login v2

--- front/webApp/src/app/app.module.ts
+++ front/webApp/src/app/app.module.ts
@@ -4,18 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { FormComponent } from './login/form/form.component';
 @NgModule({
   declarations: [
     AppComponent,
     FormComponent,
+    
    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   
   ],
   
--- front/webApp/src/app/login/form/form.component.ts
+++ front/webApp/src/app/login/form/form.component.ts
@@ -1,13 +1,33 @@
 import {Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 import { AuthService } from '../google-sign-in/google-sign-in.component';
 // import { AuthService } from '../facebook-sign-in/facebook-sign-in.component';
+import { OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
+
+export function matchPasswordsValidator(password: string, confirmPassword: string): ValidatorFn {
+  return (formGroup: AbstractControl) => {
+    const passwordControl = formGroup.get(password);
+    const confirmPasswordControl = formGroup.get(confirmPassword);
+
+    if (!passwordControl || !confirmPasswordControl) {
+      return null;
+    }
+
+    const error = passwordControl.value === confirmPasswordControl.value ? null : { passwordMismatch: true };
+    confirmPasswordControl.setErrors(error);
+    
+    return error;
+}}
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
-export class FormComponent implements AfterViewInit {
+export class FormComponent implements AfterViewInit, OnInit {
+  constructor(private authService: AuthService, private fb: FormBuilder, private http: HttpClient) { }
+  
 
   @ViewChild('container') container!: ElementRef;
   @ViewChild('register') registerBtn!: ElementRef;
@@ -22,7 +42,7 @@ export class FormComponent implements AfterViewInit {
       this.container.nativeElement.classList.remove('active');
     });
   }
-  constructor(private authService: AuthService) { }
+  
 
   onGoogleSignIn(): void {
     this.authService.signInWithGoogle().then(googleUser => {
@@ -38,4 +58,26 @@ export class FormComponent implements AfterViewInit {
   //     console.error("Error during Facebook sign-in:", error);
   //   });
   // }
+
+  userForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.userForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required]
+    }, { validator: matchPasswordsValidator('password', 'confirmPassword') });
+  }
+
+  
+
+  onSubmit(): void {
+    if (this.userForm.valid) {
+      this.http.post('http://localhost:8000/users', this.userForm.value)
+        .subscribe(response => {
+          console.log('User added:', response);
+        });
+    }
+  }
+
 }

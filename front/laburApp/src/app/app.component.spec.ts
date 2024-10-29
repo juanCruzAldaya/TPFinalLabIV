ec25da7c84058f17f988ae8a59af160e95214335
agus.quaglia51 <agustinquagliarella@gmail.com>
2024-10-29 15:26:14 -0300

    inicializando app

--- front/laburApp/src/app/app.component.spec.ts
+++ front/laburApp/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'services-market' title`, () => {
+  it(`should have the 'laburApp' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('services-market');
+    expect(app.title).toEqual('laburApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, services-market');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, laburApp');
   });
 });

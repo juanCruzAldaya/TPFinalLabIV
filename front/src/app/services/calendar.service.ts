70a4aad5a25cfd8dc1cdf3decf010eb6d1fab356
agus.quaglia51 <agustinquagliarella@gmail.com>
2024-11-09 11:45:47 -0300

    add env variable in services

--- front/src/app/services/calendar.service.ts
+++ front/src/app/services/calendar.service.ts
@@ -1,66 +1,62 @@
-import { Injectable, } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { CalendarEvent } from 'angular-calendar';
-import { map } from 'rxjs/operators';
-import {Calendario} from '../interfaces/calendario.interface'
-
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { CalendarEvent } from "angular-calendar";
+import { map } from "rxjs/operators";
+import { ICalendario } from "../interfaces/calendario.interface";
+import { environment } from "../../enviroments/enviroments";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class CalendarService {
-  private apiUrl = 'http://127.0.0.1:8000/';
+  private apiUrl = `${environment.LOCAL_API_URL}`;
 
   constructor(private http: HttpClient) {}
 
-  getCalendar(userId: number): Observable<Calendario> {
-    return this.http.get<Calendario>(`${this.apiUrl}calendarios/${userId}`);
+  getCalendar(userId: number): Observable<ICalendario> {
+    return this.http.get<ICalendario>(`${this.apiUrl}/calendarios/${userId}`);
   }
- 
 
   createEvent(event: CalendarEvent): Observable<CalendarEvent> {
-    return this.http.post<CalendarEvent>(`${this.apiUrl}eventos`, event);
+    return this.http.post<CalendarEvent>(`${this.apiUrl}/eventos`, event);
   }
 
   updateEvent(event: CalendarEvent): Observable<CalendarEvent> {
-    return this.http.put<CalendarEvent>(`${this.apiUrl}eventos/${event.id}`, event);
+    return this.http.put<CalendarEvent>(
+      `${this.apiUrl}/eventos/${event.id}`,
+      event
+    );
   }
 
   deleteEvent(eventId: number): Observable<void> {
-    return this.http.delete<void>(`${this.apiUrl}eventos/${eventId}`);
+    return this.http.delete<void>(`${this.apiUrl}/eventos/${eventId}`);
   }
-
-
-
-
 
   getAvailableSlots(profesionalId: number, date: string): Observable<string[]> {
     return this.getCalendar(profesionalId).pipe(
-      map(calendario => {
+      map((calendario) => {
         const formattedDate = date;
-        const events = calendario.eventos.filter(event => event.fecha === formattedDate);
-        const blockedTimes = events.map(event => event.hora_inicio); // Asume que cada evento tiene una propiedad 'hora_inicio'
+        const events = calendario.eventos.filter(
+          (event) => event.fecha === formattedDate
+        );
+        const blockedTimes = events.map((event) => event.hora_inicio); // Asume que cada evento tiene una propiedad 'hora_inicio'
         const allPossibleTimes = this.generatePossibleTimes();
-        return allPossibleTimes.filter(time => !blockedTimes.includes(time));
+        return allPossibleTimes.filter((time) => !blockedTimes.includes(time));
       })
     );
   }
-  
+
   private generatePossibleTimes(): string[] {
     const startHour = 9;
     const endHour = 17;
     const times: string[] = [];
-  
+
     for (let hour = startHour; hour < endHour; hour++) {
       times.push(`${hour}:00`);
       times.push(`${hour}:30`);
     }
-  
+
     return times;
   }
-
-
 }
-
-
--- front/src/app/services/filter-services.service.ts
+++ front/src/app/services/filter-services.service.ts
@@ -1,29 +1,30 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { Servicio } from '../interfaces/servicio.interface';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { IServicio } from "../interfaces/servicio.interface";
+import { environment } from "../../enviroments/enviroments";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class FilterServicesService {
-  private apiUrl = 'http://127.0.0.1:8000/servicios'; // Use HTTP for local development
+  private apiUrl = `${environment.LOCAL_API_URL}`; // Use HTTP for local development
 
   constructor(private http: HttpClient) {}
 
-  getServices(): Observable<Servicio[]> {
-    return this.http.get<Servicio[]>(this.apiUrl);
+  getServices(): Observable<IServicio[]> {
+    return this.http.get<IServicio[]>(`${this.apiUrl}/servicios`);
   }
 
   getCategoria(subCategoriaId: number): Observable<any> {
-    return this.http.get(`http://127.0.0.1:8000/categorias/${subCategoriaId}`);
+    return this.http.get(`${this.apiUrl}/categorias/${subCategoriaId}`);
   }
 
   getSubCategoria(subCategoriaId: number): Observable<any> {
-    return this.http.get(`http://127.0.0.1:8000/subcategorias/${subCategoriaId}`);
+    return this.http.get(`${this.apiUrl}/subcategorias/${subCategoriaId}`);
   }
 
   getProfesional(profesionalId: number): Observable<any> {
-    return this.http.get(`http://127.0.0.1:8000/usuarios/${profesionalId}`);
+    return this.http.get(`${this.apiUrl}/usuarios/${profesionalId}`);
   }
 }

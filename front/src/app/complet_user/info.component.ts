2da5bae9d40560c00edff974f1cfd87472e51e45
Martin Del Ollio <110709751+martindelollio@users.noreply.github.com>
2024-11-11 17:10:37 -0300

    validadores del form de informacion personalizada terminado

--- front/src/app/complet_user/info.component.ts
+++ front/src/app/complet_user/info.component.ts
@@ -1,8 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { CompletingUsersService } from '../services/completing-users.service';
 import { Usuarios } from "../interfaces/users.interfaces";
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { AuthService } from '../services/auth.services';
+
+
+export function mayorEdad(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const fechaNacimiento = new Date(control.value);
+    const fechaActual = new Date();
+    
+    let edad = fechaActual.getFullYear() - fechaNacimiento.getFullYear();
+    
+
+    const mes = fechaActual.getMonth() - fechaNacimiento.getMonth();
+    if (mes < 0 || (mes === 0 && fechaActual.getDate() < fechaNacimiento.getDate())) {
+      edad--;
+    }
+    
+    return edad >= 18 ? null : { mayorEdad: true };
+  };
+}
+export function validarTelefono(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const telefono = control.value;
+    // Verifica que el número tenga exactamente 10 dígitos
+    const esValido = /^\d{10}$/.test(telefono);
+    return esValido ? null : { telefonoInvalido: true };
+  };
+}
+export function soloLetras(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const soloLetrasRegex = /^[a-zA-Z\s]+$/;
+    const esValido = soloLetrasRegex.test(control.value);
+    return esValido ? null : { soloLetras: true };
+  };
+}
+
 
 @Component({
   selector: 'app-info',
@@ -27,10 +61,10 @@ export class InfoComponent implements OnInit {
   initializeForm(): void {
     this.formGroup = this.fb.group({
       id: [0],
-      nombre: ['', Validators.required],
-      apellido: ['', Validators.required],
-      contacto: ['', Validators.required],
-      nacimiento: ['', Validators.required],
+      nombre: ['', [Validators.required, soloLetras()]],
+      apellido: ['', [Validators.required, soloLetras()]],
+      contacto: ['', [Validators.required, validarTelefono()]],
+      nacimiento: ['', [Validators.required,mayorEdad()]],
       ciudad: ['', Validators.required],
       calificacion_promedio: [0]
     });

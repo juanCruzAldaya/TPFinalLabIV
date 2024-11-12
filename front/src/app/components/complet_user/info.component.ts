4bb0785cad027a1f5dca56a493354de1427b8f98
Martin Del Ollio <110709751+martindelollio@users.noreply.github.com>
2024-11-11 22:29:08 -0300

    mejoras en complet_user

--- front/src/app/components/complet_user/info.component.ts
+++ front/src/app/components/complet_user/info.component.ts
@@ -1,13 +1,47 @@
-import { Component, OnInit } from "@angular/core";
-import { FormGroup, FormBuilder, Validators } from "@angular/forms";
-import { IUsuarios } from "../../interfaces/users.interfaces";
-import { AuthService } from "../../services/auth.services";
-import { CompletingUsersService } from "../../services/completing-users.service";
+import { Component, OnInit } from '@angular/core';
+import { CompletingUsersService } from '../services/completing-users.service';
+import { Usuarios } from "../interfaces/users.interfaces";
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth.services';
+
+
+export function mayorEdad(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const fechaNacimiento = new Date(control.value);
+    const fechaActual = new Date();
+    
+    let edad = fechaActual.getFullYear() - fechaNacimiento.getFullYear();
+    
+
+    const mes = fechaActual.getMonth() - fechaNacimiento.getMonth();
+    if (mes < 0 || (mes === 0 && fechaActual.getDate() < fechaNacimiento.getDate())) {
+      edad--;
+    }
+    
+    return edad >= 18 ? null : { mayorEdad: true };
+  };
+}
+export function validarTelefono(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const telefono = control.value;
+    // Verifica que el número tenga exactamente 10 dígitos
+    const esValido = /^\d{10}$/.test(telefono);
+    return esValido ? null : { telefonoInvalido: true };
+  };
+}
+export function soloLetras(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const soloLetrasRegex = /^[a-zA-Z\s]+$/;
+    const esValido = soloLetrasRegex.test(control.value);
+    return esValido ? null : { soloLetras: true };
+  };
+}
+
 
 @Component({
-  selector: "app-info",
-  templateUrl: "./info.component.html",
-  styleUrls: ["./info.component.css"],
+  selector: 'app-info',
+  templateUrl: './info.component.html',
+  styleUrls: ['./info.component.css']
 })
 export class InfoComponent implements OnInit {
   formGroup!: FormGroup;
@@ -15,8 +49,8 @@ export class InfoComponent implements OnInit {
 
   constructor(
     private servis: CompletingUsersService,
-    private fb: FormBuilder,
-    private authService: AuthService
+    private fb: FormBuilder, 
+    private authService: AuthService,
   ) {}
 
   ngOnInit(): void {
@@ -27,39 +61,36 @@ export class InfoComponent implements OnInit {
   initializeForm(): void {
     this.formGroup = this.fb.group({
       id: [0],
-      nombre: ["", Validators.required],
-      apellido: ["", Validators.required],
-      contacto: ["", Validators.required],
-      nacimiento: ["", Validators.required],
-      ciudad: ["", Validators.required],
-      calificacion_promedio: [0],
+      nombre: ['', [Validators.required, soloLetras()]],
+      apellido: ['', [Validators.required, soloLetras()]],
+      contacto: ['', [Validators.required, validarTelefono()]],
+      nacimiento: ['', [Validators.required,mayorEdad()]],
+      ciudad: ['', Validators.required],
+      calificacion_promedio: [0]
     });
   }
 
   onSubmit(): void {
     if (this.formGroup.valid) {
-      const usuario: IUsuarios = {
+      const usuario: Usuarios = {
         id: this.authService.getUserId(),
-        email: this.authService.getUserEmail(),
-        password: this.authService.getUserPassword(),
-        nombre: this.formGroup.get("nombre")?.value,
-        apellido: this.formGroup.get("apellido")?.value,
-        contacto: this.formGroup.get("contacto")?.value,
-        nacimiento: this.formGroup.get("nacimiento")?.value,
-        ciudad: this.formGroup.get("ciudad")?.value,
-        calificacion_promedio: this.formGroup.get("calificacion_promedio")
-          ?.value,
+        nombre: this.formGroup.get('nombre')?.value,
+        apellido: this.formGroup.get('apellido')?.value,
+        contacto: this.formGroup.get('contacto')?.value,
+        nacimiento: this.formGroup.get('nacimiento')?.value,
+        ciudad: this.formGroup.get('ciudad')?.value,
+        calificacion_promedio: this.formGroup.get('calificacion_promedio')?.value
       };
       this.servis.updateUser(usuario).subscribe(
-        (data) => {
-          console.log("Usuario completado y enviado:", data);
+        data => {
+          console.log('Usuario completado y enviado:', data);
         },
-        (error) => {
-          console.error("Error al enviar el usuario:", error);
+        error => {
+          console.error('Error al enviar el usuario:', error);
         }
       );
     } else {
-      console.log("Formulario no valido");
+      console.log('Formulario no valido');
     }
   }
 }

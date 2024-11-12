e0c4b4bfd132d8e2c529367226cdf827be6a062a
agus.quaglia51 <agustinquagliarella@gmail.com>
2024-11-12 13:43:32 -0300

    agregando template literals de forma correcta

--- front/src/app/services/filter-services.service.ts
+++ front/src/app/services/filter-services.service.ts
@@ -1,27 +1,39 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { IService } from '../../app/interfaces/service.interface';
-import { environment } from '../../enviroments/enviroments';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { IService } from "../../app/interfaces/service.interface";
+import { environment } from "../../enviroments/enviroments";
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class FilterServicesService {
   constructor(private http: HttpClient) {}
 
   getServices(): Observable<IService[]> {
-    return this.http.get<IService[]>(environment.LOCAL_API_URL+'/servicios');
+    return this.http.get<IService[]>(`${environment.LOCAL_API_URL}/servicios`);
   }
 
   getCategoria(categoriaId: number): Observable<any> {
-    return this.http.get( environment.LOCAL_API_URL+`/categoria/${categoriaId}`);
+    return this.http.get(
+      `${environment.LOCAL_API_URL}/categoria/${categoriaId}`
+    );
   }
 
   getSubCategoria(subCategoriaId: number): Observable<any> {
-    return this.http.get( environment.LOCAL_API_URL+`/subcategoria/${subCategoriaId}`);
+    return this.http.get(
+      `${environment.LOCAL_API_URL}/subcategoria/${subCategoriaId}`
+    );
   }
 
   getProfesional(profesionalId: any): Observable<any> {
-    return this.http.get( environment.LOCAL_API_URL+`/usuarios/${profesionalId}`);
+    return this.http.get(
+      `${environment.LOCAL_API_URL}/usuarios/${profesionalId}`
+    );
+  }
+
+  getServicesByProfesionalId(profesionalId: number): Observable<any> {
+    return this.http.get(
+      `${environment.LOCAL_API_URL}/misServicios/${profesionalId}`
+    );
   }
 }

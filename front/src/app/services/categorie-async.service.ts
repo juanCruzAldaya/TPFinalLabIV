099db57786c63cb342c33e55ca1eac03ee70c2f0
agus.quaglia51 <agustinquagliarella@gmail.com>
2024-11-16 14:49:25 -0300

    modifying services

--- front/src/app/services/categorie-async.service.ts
+++ front/src/app/services/categorie-async.service.ts
@@ -13,9 +13,28 @@ export class CategoriesService {
   getCategories(): Promise<ICategory[] | any> {
     return this.http.get(`${environment.LOCAL_API_URL}/categorias`).toPromise();
   }
+
   getSubCategoriesById(id: number): Promise<ISubCategory[] | any> {
     return this.http
       .get(`${environment.LOCAL_API_URL}/subcategoriasById/${id}`)
+      .toPromise();
+  }
+
+  getCategory(id: number): Promise<ICategory[] | any> {
+    return this.http
+      .get(`${environment.LOCAL_API_URL}/categoria/${id}`)
+      .toPromise();
+  }
+
+  getSubCategory(id: number): Promise<ISubCategory[] | any> {
+    return this.http
+      .get(`${environment.LOCAL_API_URL}/subcategoria/${id}`)
+      .toPromise();
+  }
+
+  getSubcategories(): Promise<any> {
+    return this.http
+      .get(`${environment.LOCAL_API_URL}/subcategorias`)
       .toPromise();
   }
 }
